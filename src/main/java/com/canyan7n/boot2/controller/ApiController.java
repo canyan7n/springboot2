0676e79d594412c934213e429802e9c0cadff057
canyan7n <canyann@163.com>
2023-04-30 21:14:40 +0800

    commit on idea ,in springboot2 servletApi_

--- src/main/java/com/canyan7n/boot2/controller/ApiController.java
+++ src/main/java/com/canyan7n/boot2/controller/ApiController.java
@@ -5,7 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ：macheng
@@ -18,15 +22,18 @@ import javax.servlet.http.HttpServletRequest;
 public class ApiController {
 
     @RequestMapping("/api/req")
-    public String testReq(HttpServletRequest request){
+    public String testReq(HttpServletRequest request, HttpServletResponse response){
         request.setAttribute("msg","ReqMsg");
+        response.addCookie(new Cookie("c1","v1"));
         return "forward:/api/success";
     }
 
     @RequestMapping("/api/success")
     @ResponseBody
-    public String testApi(HttpServletRequest request){
+    public Map testApi(HttpServletRequest request,HttpServletResponse response){
+        HashMap<Object, Object> hashMap = new HashMap<>();
         Object msg = request.getAttribute("msg");
-        return msg.toString();
+        hashMap.put("msg",msg);
+        return hashMap;
     }
 }
