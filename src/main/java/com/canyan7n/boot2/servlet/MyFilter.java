541877c31c9842aa9f38e9804a51f8541effd41e
canyan7n <canyann@163.com>
2023-05-18 22:46:21 +0800

    commit on idea ,in springboot2 自定义filter 拦截静态资源

---
+++ src/main/java/com/canyan7n/boot2/servlet/MyFilter.java
@@ -0,0 +1,35 @@
+package com.canyan7n.boot2.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * @author ：macheng
+ * @version V1.0
+ * @ClassName :MyFilter
+ * @date ：2023/5/18 22:29
+ * @description：TODO
+ */
+@WebFilter(urlPatterns = "/static/*")
+@Slf4j
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("myfilter初始化");
+    }
+
+    @Override
+    public void destroy() {
+        log.info("myfilter销毁");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request,response);
+        log.info("myfilter正在工作");
+    }
+}
